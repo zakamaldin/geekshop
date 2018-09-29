9235c7c84c60a0b567a5b4f75e4130db5d909aa2
Andrey Zakamaldin <zakamaldin.andrey@gmail.com>
2018-09-29 13:06:57 +0300

    add json

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,63 +1,12 @@
 from django.shortcuts import render
-
-main_filling = {
-    'content': {
-        'img_src': ['product-1.jpg', 'product-2.jpg', 'product-3.jpg', 'product-4.jpg'],
-        'h3': 'Fishnet Chair',
-        'p': 'Seat and back with upholstery made of cold cure foam'},
-    'trends': {
-        'img_src': ['trend-1.jpg', 'trend-2.jpg', 'trend-3.jpg', 'trend-4.jpg', 'trend-5.jpg', 'trend-6.jpg'],
-        'h2': 'Fishnet Chair',
-        'p': 'Seat and back with upholstery made of cold cure foam. Steel frame,'
-             ' available in matt powder-coated black'},
-    'exclusive': {
-        'img_src': ['exclusive-1.jpg', 'exclusive-2.jpg', 'exclusive-3.jpg', 'exclusive-4.jpg'],
-        'h3': 'Fishnet Chair',
-        'p': 'Seat and back with upholstery made of cold cure foam'},
-    'slider1': {
-        'h3': 'TRENDING',
-        'h2': 'Fishnet Chair',
-        'p': 'Seat and back with upholstery made of cold cure foam. Steel frame, available in matt powder-coated black'},
-    'slider2': {
-        'h3': 'HOT DEAL',
-        'h2': 'Fishnet Chair',
-        'p': 'Seat and back with upholstery made of cold cure foam. Steel frame, available in matt powder-coated black'},
-}
-products_filling = {
-
-    'trends': {
-        'img_src': ['trend-1.jpg', 'trend-2.jpg', 'trend-3.jpg', 'trend-4.jpg', 'trend-5.jpg', 'trend-6.jpg'],
-        'h2': 'Fishnet Chair',
-        'p': 'Seat and back with upholstery made of cold cure foam. Steel frame,'
-             ' available in matt powder-coated black'},
-}
-product_info_filling = {
-
-    'trends': {
-        'img_src': ['trend-1.jpg', 'trend-2.jpg', 'trend-3.jpg'],
-        'h2': 'Fishnet Chair',
-        'p': 'Seat and back with upholstery made of cold cure foam. Steel frame,'
-             ' available in matt powder-coated black'},
-}
-
-contacts_filling = {
-
-    'info1': {
-        'city': '123',
-        'phone': '1900 - 1234 -5678',
-        'email': 'info@interior.com',
-        'address': '12 W 1st St, 90001 Los Angeles, California'},
-    'info2': {
-        'city': 'California',
-        'phone': '1900 - 1234 -5678',
-        'email': 'info@interior.com',
-        'address': '12 W 1st St, 90001 Los Angeles, California'},
-    'info3': {
-        'city': 'California',
-        'phone': '1900 - 1234 -5678',
-        'email': 'info@interior.com',
-        'address': '12 W 1st St, 90001 Los Angeles, California'},
-}
+from django.conf import settings
+import json
+import os
+path_to_json = os.path.join(settings.BASE_DIR, 'mainapp/static/mainapp/json')
+main_filling            = json.load(open(os.path.join(path_to_json, 'main.json'), 'r'))
+products_filling        = json.load(open(os.path.join(path_to_json, 'products.json'), 'r'))
+product_info_filling    = json.load(open(os.path.join(path_to_json, 'product_info.json'), 'r'))
+contacts_filling        = json.load(open(os.path.join(path_to_json, 'contacts.json'), 'r'))
 
 def main(request):
     return render(request, 'mainapp/index.html', main_filling)
