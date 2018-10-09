afb01d9a01f5669c038e7847f1ce479879a3e528
Andrey Zakamaldin <zakamaldin.andrey@gmail.com>
2018-10-09 22:29:29 +0300

    upload data to db from json using manage.py

---
+++ mainapp/management/commands/fill_db.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand
+from mainapp.models import Product, ProductCategory
+#from django.contrib.auth.models import User
+from django.conf import settings
+
+
+import json
+import os
+
+JSON_PATH = os.path.join(settings.BASE_DIR, 'mainapp/static/db/json')
+
+
+def load_from_json(file_name):
+    with open(os.path.join(JSON_PATH, file_name + '.json'), 'r', encoding='utf-8') as infile:
+        return json.load(infile)
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        categories = load_from_json('categories')
+        ProductCategory.objects.all().delete()
+        for c in categories:
+            new_category = ProductCategory(**c)
+            new_category.save()
+
+        products = load_from_json('products')
+        Product.objects.all().delete()
+        for p in products:
+            category_name = p['category']
+            category = ProductCategory.objects.get(name=category_name)
+            p['category'] = category
+            new_product = Product(**p)
+            new_product.save()
+
