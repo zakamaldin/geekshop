01fd58b8efd296a565fec7971fcbcc34f9ac6d39
Andrey Zakamaldin <zakamaldin.andrey@gmail.com>
2018-10-30 23:54:35 +0300

    start customize django admin panel

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -3,8 +3,13 @@ from mainapp.models import ProductCategory, Product
 from authapp.models import ShopUser
 # Register your models here.
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['image', 'name', 'category', 'short_desc', 'quantity', 'price']
+
+
 admin.site.register(ProductCategory)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(ShopUser)
 
 
