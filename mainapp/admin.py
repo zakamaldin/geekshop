5bfdb68315811b2af8051e398a6a1d6a1454ab69
Andrey Zakamaldin <zakamaldin.andrey@gmail.com>
2018-10-31 23:18:16 +0300

    start customize django admin panel for product

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,15 +1,73 @@
 from django.contrib import admin
 from mainapp.models import ProductCategory, Product
 from authapp.models import ShopUser
+from django.utils.timezone import now, timedelta
+from django.template.loader import render_to_string
 # Register your models here.
 
-
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['image', 'name', 'category', 'short_desc', 'quantity', 'price']
+    list_display = [
+        'picture',
+        'name',
+        'is_new',
+        'category',
+        'short_desc',
+        'quantity',
+        'price',
+        'created',
+        'modified',
+    ]
+
+    list_filter = [
+        'category',
+        'created',
+        'modified',
+    ]
+
+    search_fields = [
+        'name',
+        'short_desc',
+        'quantity',
+        'price',
+
+    ]
+
+    fieldsets = [
+        (
+            None, {
+                'fields': (
+                    'name',
+                    'category'
+                )
+            }
+        ),
+        (
+            'Content', {
+                'fields': (
+                    'image',
+                    'short_desc',
+                    'description',
+                    'quantity',
+                    'price'
+                )
+            }
+        ),
+
+    ]
+
+    def picture(self, obj):
+        return render_to_string(
+            'components/picture.html',
+            {'image': obj.image.url}
+
+        )
+
+    def is_new(self, obj):
+        return obj.created >= (now() - timedelta(days=5))
 
 
 admin.site.register(ProductCategory)
-admin.site.register(Product, ProductAdmin)
 admin.site.register(ShopUser)
 
 
--- mainapp/models.py
+++ mainapp/models.py
@@ -17,6 +17,8 @@ class Product(models.Model):
     description = models.TextField(verbose_name='подробно', blank=True)
     price = models.DecimalField(verbose_name='цена', max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(verbose_name='склад', default=0)
+    modified = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return "{} ({})".format(self.name, self.category.name)
