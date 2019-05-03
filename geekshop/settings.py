29117d9934110c5f892ef946294268179bdd4ed3
zakamaldin <zakamaldin.andrey@gmail.com>
2019-05-03 19:20:08 +0300

    Update settings.py

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'authapp',
     'cart',
     'rest_framework',
-    'social_django',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
